92f660e32cb5e2e5c559f52ed485e6294441da32
github username <github email address>
2018-01-28 16:44:50 -0500

    Done.

--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const PetsShow = ({ pet }) => 
+const PetsShow = ({ pet }) =>
   <div className="col-md-8">
     <h2>{pet.name}</h2>
     <p>{pet.description}</p>
   </div>;
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    pet: {}
-  };
+  console.log(ownProps)
+  const pet = state.pets.find(pet => pet.id === parseInt(ownProps.match.params.petId))
+
+  if (pet) {
+    return { pet };
+  } else {
+    return {
+      pet: {}
+    };
+  }
 };
 
 export default connect(mapStateToProps)(PetsShow);
